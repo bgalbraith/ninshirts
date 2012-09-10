81c11ddb3da75778eefb609ad136d26643b6dd27
Byron Galbraith <byron.galbraith@gmail.com>
2012-09-10 00:14:07 -0400

    removing unneeded paths to media files; added url route for serving media files from the dev server

--- ninshirts/urls.py
+++ ninshirts/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-#from ninshirts import settings
+from ninshirts import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,7 +8,10 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'apps.catalog.views.index'),
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
+
+    # Uncomment the next two line if running the development server:
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}),
 
     url(r'^(?P<category_tag>[a-z0-9-]+)/$',
         'apps.catalog.views.category'),
